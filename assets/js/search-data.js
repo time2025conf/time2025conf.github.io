b36f62e0f6ac45dfb10e228875ed526eddc9a0ba
time2025conf <impact-stet.6c@icloud.com>
2025-01-08 08:31:16 +0000

    Deploying to gh-pages from @ time2025conf/time2025conf.github.io@581246d72ac69ca192334494f9ee68fcc68f3895 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,21 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
-        },{id: "dropdown-news",
+  },{id: "dropdown-news",
               title: "News",
               description: "",
               section: "Dropdown",
