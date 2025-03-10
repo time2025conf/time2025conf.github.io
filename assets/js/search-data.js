5bc957650bd19bff6fd1225ca9c730b3a9f06b9a
PWalega <p.a.walega@gmail.com>
2025-03-10 16:45:31 +0000

    Deploying to gh-pages from @ time2025conf/time2025conf.github.io@e6e68cd437956d031ac0f78288b189b63ace2e78 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-32st-international-symposium-on-temporal-representation-and-reasoning",
-    title: "32st International Symposium on Temporal Representation and Reasoning",
+    id: "nav-introduction",
+    title: "Introduction",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
